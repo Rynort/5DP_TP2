85c72a25ecbeb35e8831ef14854d70ddfd895f47
Rynort <hahasnake11@hotmail.com>
2023-11-14 11:06:27 -0500

    Début de code

--- TP2/src/main.cpp
+++ TP2/src/main.cpp
@@ -1,9 +1,49 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <PID_v1.h>
 
-void setup() {
-  
+// Remplacez par vos propres SSID et mot de passe
+const char* ssid = "Votre_SSID";
+const char* password = "Votre_mot_de_passe";
+
+ESP8266WebServer server(80);
+
+// Paramètres PID
+double Setpoint, Input, Output;
+double Kp = 2, Ki = 5, Kd = 1;
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+
+// Remplacez par votre propre code pour lire la température
+double readTemperature() {
+    // Code pour lire la température
+    return 0.0; // Valeur fictive
 }
 
-void loop() {
-  
+void controlHeater(double output) {
+    int relayState = output > 0 ? HIGH : LOW;
+    digitalWrite(D1, relayState); // D1 est le pin du relais
+    delay(output); // Durée d'activation du relais
+    digitalWrite(D1, LOW);
+    delay(1000 - output); // Durée de désactivation du relais
+}
+
+void handleRoot() {
+    double currentTemp = readTemperature();
+    server.send(200, "text/html", "<html><body><h1>Température actuelle : " + String(currentTemp, 2) + "</h1></body></html>");
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  WiFi.softAP(ssid, password);
+  server.on("/", handleRoot);
+  server.begin();
+
+  Setpoint = 43;
+  myPID.SetMode(AUTOMATIC);
+
+  pinMode(D1, OUTPUT); // Configure
+  //À complété
 }
