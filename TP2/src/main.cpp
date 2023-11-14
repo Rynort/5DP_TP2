4b3d5fdf42a684cfc2cdd49d9d118e96bdd28567
ChaosBunny <powersound34@gmail.com>
2023-11-14 11:45:50 -0500

    Ajout Temperature equation

--- TP2/src/main.cpp
+++ TP2/src/main.cpp
@@ -5,8 +5,8 @@
 #include <PID_v1.h>
 
 // Remplacez par vos propres SSID et mot de passe
-const char* ssid = "Votre_SSID";
-const char* password = "Votre_mot_de_passe";
+const char *ssid = "Votre_SSID";
+const char *password = "Votre_mot_de_passe";
 
 ESP8266WebServer server(80);
 
@@ -15,54 +15,65 @@ double Setpoint, Input, Output;
 double Kp = 2, Ki = 5, Kd = 1;
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
+double TempConversion(int valeur)
+{
+    double voltage = valeur * 5.0 / 1023.0;
+    double resistance = 10000.0 * voltage / (5.0 - voltage);
+    double tempK = 1.0 / (1.0 / 298.15 + log(resistance / 10000.0) / 3977); // B (beta) - 3977
+    double tempCelcius = tempK - 273.15;
+    return tempCelcius;
+}
 // Remplacez par votre propre code pour lire la température
-double readTemperature() {
-    // Code pour lire la température
-    return 0.0; // Valeur fictive
+double readTemperature()
+{
+    return TempConversion(analogRead(A0));
 }
 
-void controlHeater(double output) {
+void controlHeater(double output)
+{
     int relayState = output > 0 ? HIGH : LOW;
     digitalWrite(D1, relayState); // D1 est le pin du relais
-    delay(output); // Durée d'activation du relais
+    delay(output);                // Durée d'activation du relais
     digitalWrite(D1, LOW);
     delay(1000 - output); // Durée de désactivation du relais
 }
 
-void handleRoot() {
+void handleRoot()
+{
     double currentTemp = readTemperature();
     server.send(200, "text/html", "<html><body><h1>Température actuelle : " + String(currentTemp, 2) + "</h1></body></html>");
 }
 
-void setup() {
-  Serial.begin(115200);
+void setup()
+{
+    Serial.begin(115200);
 
-  WiFi.softAP(ssid, password);
-  server.on("/", handleRoot);
-  server.begin();
+    WiFi.softAP(ssid, password);
+    server.on("/", handleRoot);
+    server.begin();
 
-  Setpoint = 43;
-  myPID.SetMode(AUTOMATIC);
+    Setpoint = 43;
+    myPID.SetMode(AUTOMATIC);
 
-  pinMode(D1, OUTPUT); // Configure
-  //À complété
+    pinMode(D1, OUTPUT); // Configure
+                         // À complété
 }
 
-void loop() {
-  Input = readTemperature();
-  myPID.Compute();
-  controlHeater(Output);
+void loop()
+{
+    Input = readTemperature();
+    myPID.Compute();
+    controlHeater(Output);
 
-  // Gestion des requêtes Web
-  server.handleClient();
+    // Gestion des requêtes Web
+    server.handleClient();
 
-  // Affichage sur le port série
-  Serial.print("Température: ");
-  Serial.print(Input, 2);
-  Serial.print("\t\t");
-  Serial.print("Chauffage: ");
-  Serial.println(Output);
+    // Affichage sur le port série
+    Serial.print("Température: ");
+    Serial.print(Input, 2);
+    Serial.print("\t\t");
+    Serial.print("Chauffage: ");
+    Serial.println(Output);
 
-  delay(1000); // Pause d'une seconde
+    delay(1000); // Pause d'une seconde
 }
-
