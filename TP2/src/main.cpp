c7ccdb9a98fce2d9a8d0cd048dc24af6206c7940
David Belisle <79233327+DavidBelisle@users.noreply.github.com>
2023-11-20 11:46:17 -0500

    cum it

--- TP2/src/main.cpp
+++ TP2/src/main.cpp
@@ -20,17 +20,18 @@ float currentTime{0};
 
 String GetContentType(String filename)
 {
-  struct Mime {
+  struct Mime
+  {
     String ext, type;
   } mimeType[] = {
-    {".html","text/html"},
-    {".js","application/javascript"},
-    {".css","text/css"},
-    {".wasm","application/wasm"}};
+      {".html", "text/html"},
+      {".js", "application/javascript"},
+      {".css", "text/css"},
+      {".wasm", "application/wasm"}};
 
   for (int i = 0; i < sizeof(mimeType) / sizeof(Mime); i++)
   {
-    if(filename.endsWith(mimeType[i].ext))
+    if (filename.endsWith(mimeType[i].ext))
     {
       return mimeType[i].type;
     }
@@ -41,84 +42,75 @@ String GetContentType(String filename)
 
 double TempConversion(int valeur)
 {
-    double voltage = valeur * 5.0 / 1023.0;
-    double resistance = 10000.0 * voltage / (5.0 - voltage);
-    double tempK = 1.0 / (1.0 / 298.15 + log(resistance / 10000.0) / 3977); // B (beta) - 3977
-    double tempCelcius = tempK - 273.15;
-    return tempCelcius;
+  double voltage = valeur * 5.0 / 1023.0;
+  double resistance = 10000.0 * voltage / (5.0 - voltage);
+  double tempK = 1.0 / (1.0 / 298.15 + log(resistance / 10000.0) / 3977); // B (beta) - 3977
+  double tempCelcius = tempK - 273.15;
+  return tempCelcius;
 }
 // Remplacez par votre propre code pour lire la température
 double readTemperature()
 {
-    return TempConversion(analogRead(A0));
+  return TempConversion(analogRead(A0));
 }
 
 void controlHeater()
 {
-    int relayState = Output > 0 ? HIGH : LOW;
-    digitalWrite(D1, relayState); // D1 est le pin du relais
-    digitalWrite(D1, LOW);
-}
-void HandleFileRequest()
-{
-    String filename = httpd.uri();
-
-  if (filename.endsWith("/"))
-    filename = "index.html";
-
-  if (LittleFS.exists(filename))
-  {
-    File file = LittleFS.open(filename, "r");
-    httpd.streamFile(file, GetContentType(filename));
-    file.close();
-  }
-  else
-  {
-    httpd.send(404, "text/plain", "404 Not Found!");
-  }
+  int relayState = Output > 0 ? HIGH : LOW;
+  digitalWrite(D1, relayState); // D1 est le pin du relais
+  digitalWrite(D1, LOW);
 }
 
 void handleRoot()
 {
-    double currentTemp = readTemperature();
-    httpd.send(200, "text/html", "<html> <head><meta http-equiv=\"refresh\" content=\"1\"></head> <body><h1>Température actuelle : " + String(currentTemp, 2) + "</h1></body></html>");
+  double currentTemp = readTemperature();
+
+  String reponse = "<html>";
+  reponse += "<head><meta http-equiv=\"refresh\" content=\"30\"></head>";
+  reponse += "<body>";
+  reponse += "<a href=\"/led?action=on\">ON</a>";
+  reponse += "<a href=\"/led?action=off\">OFF</a>";
+  reponse += "<h1>Température actuelle : " + String(currentTemp, 2) + "</h1>";
+  reponse += "";
+  reponse += "</body></html>";
+
+  httpd.send(200, "text/html", reponse.c_str());
 }
 
 void setup()
 {
-    Serial.begin(115200);
+  Serial.begin(115200);
 
-    WiFi.softAP(ssid, password);
-    httpd.on("/", handleRoot);
-    httpd.begin();
+  WiFi.softAP(ssid, password);
+  httpd.on("/", handleRoot);
+  httpd.begin();
 
-    myPID.SetMode(AUTOMATIC);
+  myPID.SetMode(AUTOMATIC);
 
-    pinMode(D1, OUTPUT);
+  pinMode(D1, OUTPUT);
 }
 
 void loop()
 {
-    if (millis() - currentTime > 1000)
-    {
-        currentTime = millis();
+  if (millis() - currentTime > 1000)
+  {
+    currentTime = millis();
 
-        Input = readTemperature();
-        myPID.Compute();
-        controlHeater();
-        digitalWrite(D1, Output);
+    Input = readTemperature();
+    myPID.Compute();
+    controlHeater();
+    digitalWrite(D1, Output);
 
-        // Gestion des requêtes Web
-        httpd.handleClient();
+    // Gestion des requêtes Web
+    httpd.handleClient();
 
-        // Affichage sur le port série
-        Serial.print("Température: ");
-        Serial.print(Input, 2);
-        Serial.print("\t\t");
-        Serial.print("Chauffage: ");
-        Serial.println(Output);
+    // Affichage sur le port série
+    Serial.print("Température: ");
+    Serial.print(Input, 2);
+    Serial.print("\t\t");
+    Serial.print("Chauffage: ");
+    Serial.println(Output);
 
-        httpd.send(200, "text/html", "<html><body><h1>Température actuelle : " + String(Input, 2) + "</h1></body></html>");
-        
-    }
+    httpd.send(200, "text/html", "<html><body><h1>Température actuelle : " + String(Input, 2) + "</h1></body></html>");
+  }
 }
