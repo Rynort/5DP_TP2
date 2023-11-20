32f65d204e417ee7b407515b7d48fbdc9613d5e5
ChaosBunny <powersound34@gmail.com>
2023-11-20 13:51:03 -0500

    Update main.cpp

--- TP2/src/main.cpp
+++ TP2/src/main.cpp
@@ -57,6 +57,11 @@ void handleRoot()
 {
   double currentTemp = readTemperature();
 
+  if (currentTemp >= 41 && currentTemp <= 45)
+  {
+    stableTime = millis();
+  }
+
   String reponse = "<html>";
   reponse += "<head><meta http-equiv=\"refresh\" content=\"30\"></head>";
   reponse += "<body>";
@@ -68,7 +73,7 @@ void handleRoot()
   reponse += "<h1>Température maximale 2 minutes: " + String(*std::max_element(std::begin(twoMin), std::end(twoMin))) + "</h1>";
   reponse += "<h1>Température minimale 5 minutes: " + String(*std::min_element(std::begin(fiveMin), std::end(fiveMin))) + "</h1>";
   reponse += "<h1>Température maximale 5 minutes: " + String(*std::max_element(std::begin(fiveMin), std::end(fiveMin))) + "</h1>";
-  reponse += "<h1>Temp de stabilité: " + String(stableTime) + "</h1>";
+  reponse += "<h1>Temp de stabilité: " + String(millis() - stableTime) + "</h1>";
   reponse += "</body></html>";
 
   httpd.send(200, "text/html", reponse.c_str());
@@ -80,7 +85,7 @@ void setup()
   WiFi.softAP(ssid, password);
   httpd.on("/", handleRoot);
   httpd.begin();
-
+  stableTime = millis();
   myPID.SetMode(AUTOMATIC);
 
   pinMode(D1, OUTPUT);
